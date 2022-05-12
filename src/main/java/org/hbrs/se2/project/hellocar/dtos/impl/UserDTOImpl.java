934d3c67aeaa939faaa8308c21419a45a1ec33d8
joergen-opsahl <93379354+joergen-opsahl@users.noreply.github.com>
2022-05-12 14:50:54 +0200

    RegistrationView Variablennamen angepasst und eine Basicoberfläche gestaltet. RegistrationControl erstellt anhand von anderem Control Objekt mit basic functionality. UserDTO um einige setter und getter erweitert. UserDTOImpl um einige getter erweitert. Basic functionality für eine User Factory erstellt.

--- src/main/java/org/hbrs/se2/project/hellocar/dtos/impl/UserDTOImpl.java
+++ src/main/java/org/hbrs/se2/project/hellocar/dtos/impl/UserDTOImpl.java
@@ -3,6 +3,7 @@ package org.hbrs.se2.project.hellocar.dtos.impl;
 import org.hbrs.se2.project.hellocar.dtos.RolleDTO;
 import org.hbrs.se2.project.hellocar.dtos.UserDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class UserDTOImpl implements UserDTO {
@@ -10,11 +11,18 @@ public class UserDTOImpl implements UserDTO {
     private int id;
     private String firstname;
     private String lastname;
+    private String email;
+    private String password; // TODO: make this more secure
+    private String dateofBirth;
     private List<RolleDTO> roles;
 
     public void setId(int id) {
         this.id = id;
     }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public void setPassword(String password) { this.password = password; }
 
     public void setFirstname(String firstname) {
         this.firstname = firstname;
@@ -23,6 +31,8 @@ public class UserDTOImpl implements UserDTO {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    public void setDateofBirth(String dateofBirth) { this.dateofBirth = dateofBirth; }
 
     public void setRoles(List<RolleDTO> roles) {
         this.roles = roles;
@@ -48,6 +58,15 @@ public class UserDTOImpl implements UserDTO {
     public List<RolleDTO> getRoles() {
         return this.roles;
     }
+
+    @Override
+    public String getEmail() { return this.email; }
+
+    @Override
+    public String getPassword() { return this.password; }
+
+    @Override
+    public LocalDate getDateofBirth() { return null; }
 
     @Override
     public String toString() {
