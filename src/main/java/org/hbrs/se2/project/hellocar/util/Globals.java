3bf2d8c9f2321a23cb1c2ea317018ffd56e9256a
Staelle Cabrelle Dzeutouo Tining <staelle.dzeutouo@smail.inf.h-brs.de>
2022-06-20 14:58:33 +0200

    Button Implementierung

--- src/main/java/org/hbrs/se2/project/hellocar/util/Globals.java
+++ src/main/java/org/hbrs/se2/project/hellocar/util/Globals.java
@@ -1,7 +1,5 @@
 package org.hbrs.se2.project.hellocar.util;
 
-import com.vaadin.flow.component.Component;
-
 public class Globals {
     public static String CURRENT_USER = "current_User";
 
@@ -9,7 +7,7 @@ public class Globals {
         public static final String REGISTRATION_VIEW = "registration";
         public static final String LANDING_PAGE_COMPANY_VIEW = "landing-page-company";
         public static final String LANDING_PAGE_STUDENT_VIEW = "landing-page-student";
-        public static final String JOB_APPLICATION_VIEW = "job-application";
+        public static final String JOB_APPLICATION_VIEW = LANDING_PAGE_STUDENT_VIEW+ "/job-detail";
         public static final String PROFIL_VIEW = "profil";
 
         public static final String LOGIN_VIEW = "login";
