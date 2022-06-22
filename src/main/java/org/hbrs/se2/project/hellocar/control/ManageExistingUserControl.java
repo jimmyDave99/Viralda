11ee67546d662702e51bc3fad4301f776e617de2
joergen-opsahl <93379354+joergen-opsahl@users.noreply.github.com>
2022-06-22 23:15:34 +0200

    Added additional Method for changing only the password of a user

--- src/main/java/org/hbrs/se2/project/hellocar/control/ManageExistingUserControl.java
+++ src/main/java/org/hbrs/se2/project/hellocar/control/ManageExistingUserControl.java
@@ -5,14 +5,25 @@ import org.hbrs.se2.project.hellocar.dtos.UserDTO;
 import org.hbrs.se2.project.hellocar.services.db.exceptions.DatabaseLayerException;
 import org.springframework.stereotype.Component;
 
+import java.security.NoSuchAlgorithmException;
+
+import static org.hbrs.se2.project.hellocar.control.RegistrationControl.hashPassword;
+
 @Component
 public class ManageExistingUserControl {
     UserDAO userDAO = new UserDAO();
 
-    public boolean updateUser(UserDTO userDTO) throws DatabaseLayerException {
+    public boolean updateUser(UserDTO userDTO, UserDTO userDTOSession) throws DatabaseLayerException {
         //if-Abfragen, was muss geupdated werden
 
         userDAO.updateUserByEmail(userDTO);
+        return true;
+    }
+
+    public boolean updateUserPassword(UserDTO userDTO, UserDTO userDTOSession) throws DatabaseLayerException,
+                NoSuchAlgorithmException {
+
+        userDAO.updateUserPasswordByEmail(userDTOSession.getEmail(), hashPassword(userDTO.getPassword()));
         return true;
     }
 
